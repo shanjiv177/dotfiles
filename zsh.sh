2cc965445597bc274e4c7c7153c677f7e13e9db4
Shanjiv A <shanjiv177@gmail.com>
2024-04-16 11:53:48 +0530

    Added zsh script

--- zsh.sh
+++ zsh.sh
@@ -2,8 +2,6 @@ sudo apt update && sudo apt upgrade
 
 sudo apt install zsh git curl
 
-# Install Oh My Zsh
-
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
